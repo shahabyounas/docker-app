2169776fbcd5f4e26620bea133b067132e61593f
shahabyounas <younasshahab@gmail.com>
2019-10-07 00:27:40 +0500

    add nodemon and npm script

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require("express")
 
 const app = express()
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
-app.get('/', (req,res) => res.send("Dockarize Hello World Node Express JS Application"))
+app.get('/', (req,res) => res.send("Dockarize Basic Node Express JS Application"))
+
+app.get("/docker", (req, res) => { res.send("hello from docker") });
+
+app.get('/nodemon', (req, res) => res.send('hello from nodemon'))
 
 app.listen(port, ()=> console.log(`Listening on port ${port}`))
